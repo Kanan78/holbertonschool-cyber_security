2b1c5b08eb611598cfbdbc4015f98976dabe7519
kanan78 <kmusayev27@gmail.com>
2025-12-25 20:24:38 +0000

    commit

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois holbertonstudents.com | awk -F: '/^(Registrant|Admin|Tech)/{if($1~/Ext/)print $1 ":," $2; else print $1 "," $2}'
+whois $1 | awk -F: '/^(Registrant|Admin|Tech)/{if($1~/Ext/)print $1 ":," $2; else print $1 "," $2}'
