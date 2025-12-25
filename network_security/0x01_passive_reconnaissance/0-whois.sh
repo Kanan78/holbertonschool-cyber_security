efa3a94a7333659c51f1d17bcadde5286afa2c6e
kanan78 <kmusayev27@gmail.com>
2025-12-25 14:53:37 +0000

    commit

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk '/Registrant|Admin|Tech/ {print $1, $2, $3, $4}' >> $1.txt
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech)[[:space:]]/{f=$1;gsub(/[[:space:]]+/,"",f);v=$2;sub(/^[ \t]+/,"",v);if(f~/Street/)v=v" ";if(f~/Ext/&&v=="")v=":";printf "%s,%s%s",f,v,ORS} END{printf ""}'
