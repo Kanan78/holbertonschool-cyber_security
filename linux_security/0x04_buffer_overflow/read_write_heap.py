de37fa0032a8e930eeebfb30e0e200ebc64839bc
kanan78 <kmusayev27@gmail.com>
2026-04-03 16:43:39 +0400

    Script

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
 """
-Finds and replaces a string in the heap of a running process.
-Usage: read_write_heap.py pid search_string replace_string
+This module provides a script to find and replace a string in the heap
+of a running process. It accesses /proc/[pid]/maps and /proc/[pid]/mem.
 """
 import sys
 
-if len(sys.argv) != 4:
-    sys.exit(1)
 
-pid, search, replace = sys.argv[1:]
+def read_write_heap():
+    """
+    Finds and replaces a string in the heap of a running process.
+    Usage: read_write_heap.py pid search_string replace_string
+    """
+    if len(sys.argv) != 4:
+        print("Usage: read_write_heap.py pid search replace")
+        sys.exit(1)
 
-try:
-    with open("/proc/{}/maps".format(pid), "r") as f:
-        heap_line = [line for line in f if "[heap]" in line][0]
-        addr_range = heap_line.split()[0].split('-')
-        start, end = int(addr_range[0], 16), int(addr_range[1], 16)
+    pid, search, replace = sys.argv[1:]
 
-    with open("/proc/{}/mem".format(pid), "rb+") as f:
-        f.seek(start)
-        data = f.read(end - start)
-        idx = data.find(search.encode())
+    try:
+        # Open maps file to find the heap range
+        with open("/proc/{}/maps".format(pid), "r") as f:
+            heap_line = [line for line in f if "[heap]" in line][0]
+            addr_range = heap_line.split()[0].split('-')
+            start = int(addr_range[0], 16)
+            end = int(addr_range[1], 16)
 
-        if idx != -1:
-            f.seek(start + idx)
-            f.write(replace.encode())
-except Exception:
-    sys.exit(1)
+        # Open mem file to perform the search and replace
+        with open("/proc/{}/mem".format(pid), "rb+") as f:
+            f.seek(start)
+            data = f.read(end - start)
+            idx = data.find(search.encode())
+
+            if idx != -1:
+                f.seek(start + idx)
+                f.write(replace.encode())
+            else:
+                sys.exit(1)
+
+    except Exception:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    read_write_heap()
