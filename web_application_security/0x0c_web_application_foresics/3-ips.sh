7ca120ded734a9ad446c25c7772d86081440119a
kanan78 <kmusayev27@gmail.com>
2026-04-17 06:47:20 -0400

    IP

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep "Accepted password" ~/auth.log | grep "root" |  awk '{print $11}' | sort | uniq | wc -l
+grep "Accepted password" auth.log | grep "root" |  awk '{print $11}' | sort | uniq | wc -l
