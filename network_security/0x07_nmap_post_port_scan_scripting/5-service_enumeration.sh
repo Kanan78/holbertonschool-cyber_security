fe87d5b2bff8670ecfebf0fd432dfbac185e0146
kanan78 <kmusayev27@gmail.com>
2026-04-25 16:47:08 +0400

    Comprehensive Network Reconnaissance

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmap -sV -A --script banner ssl-enum-ciphers smb-enum-domains $1 -oN service_enumeration_results.txt
+nmap -sV -A --script banner ssl-enum-ciphers smb-enum-domains default $1 -oN service_enumeration_results.txt
