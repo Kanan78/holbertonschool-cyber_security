217fc004a42bd636620a3b405a216efdf1b31063
kanan78 <kmusayev27@gmail.com>
2026-05-24 20:33:55 +0400

    http_request

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
-
 require 'net/http'
 require 'json'
+require 'uri'
 
 def get_request(url)
-  uri = URI('http://example.com/index.html?count=10')
+  uri = URI.parse(url)
   response = Net::HTTP.get_response(uri)
-  puts response
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts 'Response body:'
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
