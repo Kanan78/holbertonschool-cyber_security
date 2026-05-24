9ade42c016eb9727d2b33943709c2896e57d01d8
kanan78 <kmusayev27@gmail.com>
2026-05-24 19:58:15 +0400

    hello_world_class

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# HelloWorld class that stores and displays a greeting message
+class HelloWorld
+  # Initialize method sets the message instance variable
+  def initialize
+    @message = 'Hello, World!'
+  end
+
+
+  def print_hello
+    puts @message
+  end
+end
